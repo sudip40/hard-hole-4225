a0e9595be952e2f322ac57fbd26b2543ec8d6aa5
sudip40 <karmakar.sudip40@gmail.com>
2022-09-28 23:09:57 +0530

    Course Monitoring System

---
+++ Course Monitoring System/src/com/CMS/Course.java
@@ -0,0 +1,17 @@
+package com.CMS;
+
+public class Course {
+	private int coursed;
+	private String courseName;
+	private int fee;
+	private String courseDescription;
+
+	public Course(int coursed, String courseName, int fee, String courseDescription) {
+		super();
+		this.coursed = coursed;
+		this.courseName = courseName;
+		this.fee = fee;
+		this.courseDescription = courseDescription;
+	}
+
+}
